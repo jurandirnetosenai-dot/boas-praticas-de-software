2e9776680ca9223eb4e9a5b84b623f6bf32ab273
JurandirNeto <jurandirnetosenai@gmail.com>
2026-09-08 16:51:08 -0300

    Correção do arquivo, boas pratica de manunteção.

--- Sistema.java
+++ Sistema.java
@@ -1,27 +1,39 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.sistema;
 
 /**
  *
  * @author juran
  */
-public class Sistema {
-    public static void main(String[] args) {
-        String n = "Carlos";
-        double a = 8;
-        double b = 7;
-        double c = (a + b) / 2;
-        
-        System.out.println("Aluno: " + n);
-        System.out.println("Media: " + c);
-        
-        if (c >= 6) {
-            System.out.println("Aprovado");
-        } else {
-            System.out.println("Reprovado");
+
+    public class Sistema {
+
+        private static final double MEDIA_MINIMA_APROVACAO = 6;
+
+        public static void main(String[] args) {
+            String nomeAluno = "Carlos";
+            double notaPrimeiraAvaliacao = 8;
+            double notaSegundaAvaliacao = 7;
+
+            double media = calcularMedia(notaPrimeiraAvaliacao, notaSegundaAvaliacao);
+            String situacao = verificarSituacao(media);
+
+            exibirResultado(nomeAluno, media, situacao);
+        }
+
+        private static double calcularMedia(double nota1, double nota2) {
+            return (nota1 + nota2) / 2;
+        }
+
+        private static String verificarSituacao(double media) {
+            return media >= MEDIA_MINIMA_APROVACAO ? "Aprovado" : "Reprovado";
+        }
+
+        private static void exibirResultado(String nomeAluno, double media, String situacao) {
+            System.out.println("Aluno: " + nomeAluno);
+            System.out.println("Media: " + media);
+            System.out.println(situacao);
         }
     }
-}
